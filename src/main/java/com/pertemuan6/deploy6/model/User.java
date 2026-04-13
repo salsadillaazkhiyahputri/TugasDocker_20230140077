113f5d356ee673f8796a188e6bef5d88550e5215
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-04-13 11:40:32 +0700

    tambah user

---
+++ src/main/java/com/pertemuan6/deploy6/model/User.java
@@ -0,0 +1,12 @@
+package com.pertemuan6.deploy6.model;
+
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String nim;
+    private String nama;
+    private String jenisKelamin;
+}
