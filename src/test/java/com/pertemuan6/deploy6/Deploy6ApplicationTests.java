d62b3a73bcaa48b1db28e803620b95802c7c40c6
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-04-13 11:39:33 +0700

    Initial commit

---
+++ src/test/java/com/pertemuan6/deploy6/Deploy6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pertemuan6.deploy6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
