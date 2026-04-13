cc5550eee636e2e838808d3bce8d8f1a3374107d
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-04-13 11:42:32 +0700

    tambah entry point aplikasi Spring Boot

---
+++ src/main/java/com/pertemuan6/deploy6/Deploy6Application.java
@@ -0,0 +1,13 @@
+package com.pertemuan6.deploy6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy6Application.class, args);
+	}
+
+}
